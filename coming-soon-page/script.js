5a2ea469689449f5325abd2b0f1385cd0a3b6441
Math <ceraline.mathieu@gmail.com>
2020-04-20 23:44:25 -0400

    Adding form validation, error state and hover effect
    Reorganised the form element, added form validation using js and added the hover state on the social icons buttons.

--- coming-soon-page/script.js
+++ coming-soon-page/script.js
@@ -1,7 +1,21 @@
-const subscribe = () => {
-    let form = document.body.getElementsByClassName("b-subscription-form");
-    let error = document.body.getElementsByClassName("subscription-error")[0];
-    error.innerHTML = "Whoops! It looks like you forgot to add your email";
-    let hidden = error.getAttribute('hidden').valueOf();
-    console.log(hidden);
-}
+document.forms["newsletter-form"].addEventListener("submit", (event) => {
+    let email = document.forms["newsletter-form"]["email"];
+    const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let message = document.getElementById("subscription-message");
+    message.innerHTML = "Hello World!";
+
+    if (emailRegExp.test(email.value)){
+        message.innerHTML = "Thank you for subscribing! 🙏🏾";
+        message.classList.add("subscription-validation");
+    }
+    else if(email.value.length == 0){
+        message.innerHTML = "Whoops! It looks like you forgot to add your email!";
+        message.classList.add("subscription-error");
+    }
+    else{
+        message.innerHTML = "Please provide a valid email address.";
+        message.classList.add("subscription-error");
+    }    
+    message.removeAttribute("hidden");
+    event.preventDefault();
+});
